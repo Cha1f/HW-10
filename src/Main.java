27147b11fd44dae83b114c6ba3235485272a52f2
Рома <pomo4444ka@gmail.com>
2023-03-03 23:00:45 +0300

    HW-10 done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+    }
+
+    public static void task1() {
+        System.out.println();
+        String firstName = "Ivan";
+        String middleName = "Ivanov";
+        String lastName = "Ivanovich";
+        String fullName = middleName + " " + firstName + " " + lastName;
+        System.out.println("ФИО сотрудника - " + fullName);
+    }
+
+    public static void task2() {
+        String fullName = "Ivanov Ivan Ivanovich";
+        String small = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + small.toUpperCase());
+    }
+
+    public static void task3() {
+        String fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
+        System.out.println(fullName);
     }
 }
